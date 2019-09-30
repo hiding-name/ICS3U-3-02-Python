d6f50da081c6c08ae5029f6204bf043062432b54
Ubuntu <ubuntu@ip-172-31-18-91.ec2.internal>
2019-09-30 18:46:06 +0000

    Initial code

---
+++ guess_number.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created by: Matsuru Hoshi
+# Created on: Sept 2019
+# This is program adds numbers
+
+
+def main():
+    # funciton makes user guess nimber
+
+    # input
+    number = float(input("Guess the number: "))
+
+    # process
+    if number == 5 :
+        #output
+        print("Good job, you got it.")
+
+
+if __name__ == "__main__":
+    main()
